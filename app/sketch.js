a4f27deb648e48b3e9bd5a5168c3d59983032d63
Hub <hubmoncenis@gmail.com>
2017-09-25 17:26:46 +0200

    Get the song from an external JSON

--- app/sketch.js
+++ app/sketch.js
@@ -1,40 +1,5 @@
-var songsArray = [
-   {
-      name: "Daft Punk - Harder better faster",
-      img : "Images/daft.jpg",
-      song: "Songs/daft.mp3",
-      link: "https://www.youtube.com/channel/UCKHFvArwRwQU2VbRjMpaVGw",
-   },
-   {
-      name: "Moderat - Eating Hooks",
-      img : "Images/moderat.jpg",
-      song: "Songs/moderat.mp3",
-      link: "http://moderat.fm/",
-   },
-   {
-      name: "Vitalic - Waiting for the stars",
-      img : "Images/vitalic.jpg",
-      song: "Songs/vitalic.mp3",
-      link: "https://www.vitalic.org/",
-   },
-   {
-      name: "Calvins Harris - Feels",
-      img : "Images/feels.jpg",
-      song: "Songs/feels.mp3",
-      link: "http://new.calvinharris.com/",
-   }
-];
-
-var randomMusic = songsArray[Math.floor(Math.random() * songsArray.length)];
-
-function preload(){
-   console.log(randomMusic.name);
-   soundFormats('mp3', 'ogg');
-   var songs = ["vitalic.mp3", "daft.mp3", "moderat.mp3"];
-   var selectedSong = Math.floor(Math.random() *songs.length);
-   sound = loadSound(randomMusic.song);
-}
-
+var randomMusic ;
+var sound;
 var w = window.innerWidth;
 var h  = window.innerHeight;
 var balls = [];
@@ -52,16 +17,24 @@ var songTitle;
 var sliderSpeed;
 var sliderVolume;
 
+function preload(){
+   loadJSON('songs.json', function(data){
+	   randomMusic = data[Math.floor(Math.random() * data.length)];
+	   console.log(randomMusic);
+	   soundFormats('mp3', 'ogg');
+	   sound = loadSound(randomMusic.song);
+   });
+}
+
 function setup() {
    canvas = createCanvas(w,h);
    amplitude = new p5.Amplitude();
    sound.setVolume(0.4);
    sound.play();
-
    for(var i=0; i<80; i++) {
       balls[i] = new Ball();
    }
-
+   
 	buttonPlayPause = new buttonPlayPause();
 	buttonPlayPause.display();
 
