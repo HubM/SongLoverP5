0b742574f45dc2c2eeda4de9f89a8b80ac9ad56d
Hub <hubmoncenis@gmail.com>
2017-09-26 16:45:58 +0200

    start post

--- app/server.js
+++ app/server.js
@@ -1,20 +1,29 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+
 var path = require('path');
 
-//
-// app.use(express.static(__dirname + '/Classes'));
-// app.use(express.static(__dirname + '/Songs'));
-// app.use(express.static(__dirname + '/Images'));
-// app.use(express.static(__dirname + '/sketch.css'));
-// app.use(express.static(__dirname + '/sketch.js'));
-
-
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({
+	extended: true;
+}));
+
+app.use(bodyParser.json());
 
 app.get('/', function (req,res) {
 	res.sendFile(__dirname + '/index.html');
 });
+
+app.post('/', function(request, response){
+    console.log(request.body.songName);
+    console.log(request.body.songFile);
+	console.log(request.body.songCover);
+	console.log(request.body.artistWebsite);
+});
+
 
 app.listen(3000, function() {
 	console.log('App listening on localhost 3000');
