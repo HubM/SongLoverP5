a1ac79cb5da865dca2ef6b5a90ad3016c9156a13
Hub <hubmoncenis@gmail.com>
2017-10-17 14:38:43 +0200

    hotfix pb image file input name

--- app/public/inputFile.js
+++ app/public/inputFile.js
@@ -1,8 +1,14 @@
 function changeName(val){
 	var newFile = val.files;
 	if(newFile.length >  0) {
-		var actualLabel = document.getElementById('LabelSongFile');
-		return actualLabel.textContent = newFile[0].name;
+		if(newFile[0].type == "audio/mp3" || newFile[0].type == "audio/ogg") {
+			var actualLabel = document.getElementById('LabelSongFile');
+			return actualLabel.textContent = newFile[0].name;
+		}
+		if(newFile[0].type == "image/jpeg" || newFile[0].type == "image/png") {
+			var actualLabel = document.getElementById('LabelImgCover');
+			return actualLabel.textContent = newFile[0].name;
+		}
 	} else {
 		return;
 	}
