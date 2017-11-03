cc7598e41dc837e592a69afe932bfd9a280fef62
HubM <hubmoncenis@gmail.com>
2017-11-03 17:38:35 +0100

    fix file uploader pb

--- app/server.js
+++ app/server.js
@@ -1,26 +1,25 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const fs = require('file-system');
-const fileUpload = require('express-fileupload');
-const favicon = require('serve-favicon');
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("file-system");
+const fileUpload = require("express-fileupload");
+const favicon = require("serve-favicon");
 const app = express();
 
+var path = require("path");
 
-var path = require('path');
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, "public", "favicon.png")));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(fileUpload());
 
-app.get('/', function (req,res) {
-	res.sendFile(__dirname + '/index.html');
+app.get("/", function (req,res) {
+	res.sendFile(__dirname + "/index.html");
 });
 
-app.post('/', function(req, res){
+app.post("/", function(req, res){
 
-		var filePath = __dirname + '/public/songs.json';
+		var filePath = __dirname + "/public/songs.json";
 		var configFile = fs.readFileSync(filePath);
 		var config = JSON.parse(configFile);
 
@@ -31,14 +30,14 @@ app.post('/', function(req, res){
 			var songName = req.body.songName;
 		}
 		// verify if song has a file and the type
-		if((req.files.songFile != undefined) && (req.files.songFile.mimetype == 'audio/mp3' || req.files.songFile.mimetype == 'audio/ogg')) {
+		if((req.files.songFile != undefined) && (req.files.songFile.mimetype == "audio/mp3" || req.files.songFile.mimetype == "audio/ogg")) {
 			var songFile = req.files.songFile;
 		} else {
 			console.log("L'import n'a pas marché, le fichier audio n'est pas conforme");
 		}
 
 		// verify if the song has a picture and the type
-		if((req.files.songCover != undefined) && (req.files.songCover.mimetype == 'image/jpeg' || req.files.songCover.mimetype == 'image/png')) {
+		if((req.files.songCover != undefined) && (req.files.songCover.mimetype == "image/jpeg" || req.files.songCover.mimetype == "image/png")) {
 			var songCover = req.files.songCover;
 		}
 
@@ -48,18 +47,18 @@ app.post('/', function(req, res){
 			var artistWebsite = req.body.artistWebsite;
 		}
 
-		if(typeof songName !== 'undefined' && typeof songFile !== 'undefined' && typeof songCover !== 'undefined' 	&& artistWebsite !== '') {
+		if(typeof songName !== "undefined" && typeof songFile !== "undefined" && typeof songCover !== "undefined" 	&& artistWebsite !== "") {
 
 			// create the new JSON Object
 			var newSong = 	{	name: ""+songName+"", img:"Images/"+""+songCover.name+"", song:"Songs/"+""+songFile.name+"", link: ""+artistWebsite+""};
 
-			songFile.mv('public/Songs/'+songFile.name, function(err){
+			songFile.mv(__dirname + "/public/Songs/"+songFile.name, function(err){
 				if(err) {
 					console.log(err);
 				}
 			});
 
-			songCover.mv('public/Images/'+songCover.name, function(err){
+			songCover.mv(__dirname + "/public/Images/"+songCover.name, function(err){
 				if(err) {
 					console.log(err);
 				}
@@ -70,7 +69,7 @@ app.post('/', function(req, res){
 			var allValids = [];
 
 			for(var song in config) {
-				if(songName === config[song]['name'] || songFile === config[song]['song'] || songCover === config[song]['img']) {
+				if(songName === config[song]["name"] || songFile === config[song]["song"] || songCover === config[song]["img"]) {
 					valid = false;
 					allValids.push(valid);
 				} else {
@@ -83,7 +82,7 @@ app.post('/', function(req, res){
 			allValids.every(function(element, index){
 				if(element == false) {
 					console.log("The song has already been uploaded");
-					res.sendFile(__dirname + '/public/Views/error.html');
+					res.sendFile(__dirname + "/public/Views/error.html");
 					return statut = 0;
 				} else {
 					return true;
@@ -96,15 +95,15 @@ app.post('/', function(req, res){
 				config.push(newSong);
 				var configJSON = JSON.stringify(config);
 				fs.writeFileSync(filePath, configJSON);
-				res.sendFile(__dirname + '/public/Views/thanks.html');
+				res.sendFile(__dirname + "/public/Views/thanks.html");
 			}
 	} else {
 
 		// Si problème avec les champs du formulaire
-		res.sendFile(__dirname + '/public/Views/error.html');
+		res.sendFile(__dirname + "/public/Views/error.html");
 	}
 });
 
 app.listen(3000, function() {
-	console.log('App listening on localhost:3000');
+	console.log("App listening on localhost:3000");
 });
