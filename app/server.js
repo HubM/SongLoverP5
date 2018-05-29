6f1e1a62ddb6f4bf6801393004b9977de70696a6
Hubert Moncenis <hubmoncenis@gmail.com>
2018-05-29 21:43:12 +0200

    Refacto and recheck server file

--- app/server.js
+++ app/server.js
@@ -4,8 +4,10 @@ const fs = require("file-system");
 const fileUpload = require("express-fileupload");
 const favicon = require("serve-favicon");
 const app = express();
-
-var path = require("path");
+const path = require("path");
+const filePath = __dirname + "/public/songs.json";
+const configFile = fs.readFileSync(filePath);
+const config = JSON.parse(configFile);
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(favicon(path.join(__dirname, "public", "favicon.png")));
@@ -13,95 +15,93 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(fileUpload());
 
-app.get("/", function (req,res) {
-	res.sendFile(__dirname + "/index.html");
-});
+app.get("/", (req,res) => res.sendFile(__dirname + "/index.html"));
 
 app.post("/", function(req, res){
 
-		var filePath = __dirname + "/public/songs.json";
-		var configFile = fs.readFileSync(filePath);
-		var config = JSON.parse(configFile);
+	let songName,
+			songFile,
+			songCover,
+			artistName,
+			artistWebsite;
 
-		// give a title to the song
-		if(req.files.songName == undefined) {
-			var songName = req.files.songFile.name;
-		} else {
-			var songName = req.body.songName;
-		}
-		console.log(req.files.songFile);
-		// verify if song has a file and the type
-		if((req.files.songFile != undefined) && (req.files.songFile.mimetype == "audio/mp3" 
-				|| req.files.songFile.mimetype == "audio/ogg") 
-				|| req.files.songFile.mimetype == "audio/mpeg") {
-			var songFile = req.files.songFile;
-		} else {
-			console.log("L'import n'a pas marché, le fichier audio n'est pas conforme");
-		}
+	/* ===========
+	  SONG TITLE
+	============ */
+	((req.body.songName.length >  0) ? songName = req.body.songName : songName = 'Empty title'); 
 
-		// verify if the song has a picture and the type
-		if((req.files.songCover != undefined) && (req.files.songCover.mimetype == "image/jpeg" || req.files.songCover.mimetype == "image/png")) {
-			var songCover = req.files.songCover;
-		}
+	/* ==============
+	  SONG FILE & TYPE
+	=============== */
+	(((req.files.songFile != undefined) && (req.files.songFile.mimetype == "audio/mp3"  || req.files.songFile.mimetype == "audio/ogg" || req.files.songFile.mimetype == "audio/mpeg")) ? songFile = req.files.songFile : songFile = undefined);
+	
+	/* ===========
+	  SONG PICTURE
+	============ */
+	(((req.files.songCover != undefined) && (req.files.songCover.mimetype == "image/jpeg"  || req.files.songCover.mimetype == "image/png")) ? songCover = req.files.songCover : songCover = undefined );
 
-		if(req.body.artistWebsite == '') {
-			var artistWebsite = "/";
-		} else {
-			var artistWebsite = req.body.artistWebsite;
-		}
+	/* =====
+	  ARTIST
+	====== */		
+	((req.body.artistName.length >  0) ? artistName = req.body.artistName : artistName = "Empty artist");
 
-		if(typeof songName !== "undefined" && typeof songFile !== "undefined" && typeof songCover !== "undefined" 	&& artistWebsite !== "") {
+	/* ===========
+	  ARTIST WEBSITE
+	============ */		
+	((req.body.artistWebsite.length > 0) ? artistWebsite = req.body.artistWebsite : artistWebsite = "#");
 
-			// create the new JSON Object
-			var newSong = 	{	name: ""+songName+"", img:"Images/"+""+songCover.name+"", song:"Songs/"+""+songFile.name+"", link: ""+artistWebsite+""};
+	
+	if(typeof songFile !== undefined && typeof songCover !== undefined) {
 
-			songFile.mv(__dirname + "/public/Songs/"+songFile.name, function(err){
-				if(err) {
-					console.log(err);
-				}
-			});
+		/* =================================
+			Create new JSON Object
+			Move files to folders
+		================================== */
+		const newSong = 	{	
+			name: ""+songName+"", 
+			img:"Images/"+""+songCover.name+"", 
+			song:"Songs/"+""+songFile.name+"", 
+			link: ""+artistWebsite+""
+		};
 
-			songCover.mv(__dirname + "/public/Images/"+songCover.name, function(err){
-				if(err) {
-					console.log(err);
-				}
-			});
+		songFile.mv(__dirname + "/public/Songs/"+songFile.name, (err) => { if(err) { console.log(err); } });
+		songCover.mv(__dirname + "/public/Images/"+songCover.name, (err) => { if(err) { console.log(err); } });
 
-			var valid;
-			var statut = 1;
-			var allValids = [];
+		let valid,
+				statut = 1,
+				allValids = [];
 
-			for(var song in config) {
-				if(songName === config[song]["name"] || songFile === config[song]["song"] || songCover === config[song]["img"]) {
-					valid = false;
-					allValids.push(valid);
-				} else {
-					valid = true;
-					allValids.push(valid);
-				}
+		for(let song in config) {
+			if(songName === config[song]["name"] || songFile === config[song]["song"] || songCover === config[song]["img"]) {
+				valid = false;
+				allValids.push(valid);
+			} else {
+				valid = true;
+				allValids.push(valid);
 			}
+		}
 
-			// comparer si son valid ou non
-			allValids.every(function(element, index){
-				if(element == false) {
-					console.log("The song has already been uploaded");
-					res.sendFile(__dirname + "/public/Views/error.html");
-					return statut = 0;
-				} else {
-					return true;
-				}
-			});
-
-			// si tout ok, alors ecriture dans JSON
-			// console.log("statut : " + statut);
-			if( statut ==  1) {
-				config.push(newSong);
-				var configJSON = JSON.stringify(config);
-				fs.writeFileSync(filePath, configJSON);
-				res.sendFile(__dirname + "/public/Views/thanks.html");
+		// comparer si son valid ou non
+		allValids.every((element, index) => {
+			if(element == false) {
+				console.log("The song has already been uploaded");
+				res.sendFile(__dirname + "/public/Views/error.html");
+				return statut = 0;
+			} else {
+				return true;
 			}
+		});
+
+		// si tout ok, alors ecriture dans JSON
+		// console.log("statut : " + statut);
+		if( statut ==  1) {
+			config.push(newSong);
+			var configJSON = JSON.stringify(config);
+			fs.writeFileSync(filePath, configJSON);
+			res.sendFile(__dirname + "/public/Views/thanks.html");
+		}
 	} else {
-
+		console.log("PLEASE USE COMMONS EXTENSIONS")
 		// Si problème avec les champs du formulaire
 		res.sendFile(__dirname + "/public/Views/error.html");
 	}
